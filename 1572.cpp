b0537e427b2cfdf3ab35ce32d802c8a70dc81d01
Yuvraj Chandra <Singhyuvraj179@gmail.com>
2020-10-30 13:54:00 +0530

    Create 1572.cpp

---
+++ 1572.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int sum=0;
+        for(int i=0; i<mat.size(); i++)
+        {
+            for(int j=0; j<mat[i].size(); j++)
+            {
+                if((i==j) && ( (i+j) % mat.size()-1== 0) )
+                {
+                    sum += mat[i][j];
+                    continue;
+                }
+                if(i==j)
+                {
+                    sum += mat[i][j];
+                }
+                else if( (i+j)%(mat.size()-1) == 0)
+                {
+                    sum += mat[i][j];
+                }
+                
+            }
+        }
+        
+        return sum;
+    }
+};
